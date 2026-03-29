85c254319c57f610273a76a90f7b29574c531549
Людмила <liudmila@MacBook-Pro-Ludmila.local>
2026-03-29 18:31:44 +0300

    те що зробила

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,7 +1,9 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return children;
+export default function RootLayout({ children }) {
+  return (
+    <html lang="uk">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
 }
