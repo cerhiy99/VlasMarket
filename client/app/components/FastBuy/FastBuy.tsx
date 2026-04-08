e16ea57f31f6f802f45abf065ca799e63f50a390
Людмила <liudmila@mbp-ludmila.home>
2026-04-08 20:05:27 +0300

    fastbuy

--- client/app/components/FastBuy/FastBuy.tsx
+++ client/app/components/FastBuy/FastBuy.tsx
@@ -2,7 +2,7 @@
 import { Locale } from '@/i18n.config';
 import React, { useEffect, useRef, useState } from 'react';
 import './FastBuy.scss';
-import { IoClose } from 'react-icons/io5';
+import CloseSVG from '../../assest/Goods/Close.svg';
 import Inputmask from 'inputmask';
 import { $host } from '@/app/http';
 import { useTranslation } from '@/context/TranslationProvider';
@@ -12,7 +12,7 @@ type Props = {
   fastBuy: boolean;
   setFastBuy: Function;
   idGoods: number;
-  idVolume: string; //url замовлення*
+  idVolume: string;
   nameProduct: string;
   realIdVolume: number;
 };
@@ -28,23 +28,25 @@ const FastBuy = ({
 }: Props) => {
   const { t } = useTranslation();
   const phoneInputRef = useRef<HTMLInputElement>(null);
+
   const [number, setNumber] = useState('');
   const [name, setName] = useState('');
   const [message, setMessage] = useState('');
   const [error, setError] = useState('');
 
   useEffect(() => {
-    // Ініціалізація маски для телефону через реф
     if (phoneInputRef.current) {
       Inputmask('+380 (99) 999-99-99').mask(phoneInputRef.current);
     }
   }, [fastBuy]);
 
-  const sumbit = async (e: any) => {
+  const sumbit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
     try {
       if (message || error) return;
-      e.preventDefault();
-      const res = await $host.post('order/fastOrder', {
+
+      await $host.post('order/fastOrder', {
         name,
         phone: number,
         goodsID: idGoods,
@@ -52,19 +54,20 @@ const FastBuy = ({
         nameProduct,
         realIdVolume,
       });
+
       setMessage('Заявка успішно надіслана.');
+      setError('');
     } catch (err) {
       setError(
-        'Сталася помилка, спробуйте пізніше або зателефонуйте за номером' +
+        'Сталася помилка, спробуйте пізніше або зателефонуйте за номером ' +
           process.env.NEXT_PUBLIC_PHONE_1
       );
+      setMessage('');
       console.log(err);
     }
   };
 
-  if (!fastBuy) {
-    return false;
-  }
+  if (!fastBuy) return null;
 
   return (
     <div className="fast-buy-container">
@@ -72,10 +75,24 @@ const FastBuy = ({
         <div className="title">
           <h3>{t('fastBuy.title')}</h3>
           <div className="close" onClick={() => setFastBuy(false)}>
-            <IoClose size={25} />
+            <CloseSVG />
           </div>
         </div>
+
         <form onSubmit={sumbit} className="other">
+          <div className="row">
+            <label>
+              {t('fastBuy.name')} <span>*</span>
+            </label>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder={t('fastBuy.name') as string}
+              required
+            />
+          </div>
+
           <div className="row">
             <label>
               {t('fastBuy.phone')} <span>*</span>
@@ -85,30 +102,22 @@ const FastBuy = ({
               type="text"
               value={number}
               onChange={(e) => setNumber(e.target.value)}
-              className="phone-input"
-              placeholder="+380 (__) ___-__-__"
+              placeholder={t('fastBuy.phone') as string}
               pattern="^\+380 \(\d{2}\) \d{3}-\d{2}-\d{2}$"
               required
             />
           </div>
-          <div className="row">
-            <label>
-              {t('fastBuy.name')}
-              <span>*</span>
-            </label>
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className="phone-input"
-              placeholder={t('fastBuy.name') as string}
-              required
-            />
-          </div>
-          {message && <p style={{ color: 'green' }}>{message}</p>}
-          {error && <p style={{ color: 'red' }}>{error}</p>}
+
+          <p className="fast-buy-text">
+            Усі деталі замовлення, такі як оплата, доставка та інші умови, ми
+            погодимо з вами під час телефонної розмови.
+          </p>
+
+          {message && <p className="success-message">{message}</p>}
+          {error && <p className="error-message">{error}</p>}
+
           <div className="but">
-            <button>{t('fastBuy.buy')}</button>
+            <button type="submit">{t('fastBuy.buy')}</button>
           </div>
         </form>
       </div>
