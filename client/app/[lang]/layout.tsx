1fa20163eda11918ff6ddb3c7407c0309fa58b7d
cerhiy99 <cerhiy99@gmail.com>
2026-04-19 21:22:03 +0300

    comment script

--- client/app/[lang]/layout.tsx
+++ client/app/[lang]/layout.tsx
@@ -28,15 +28,15 @@ export default async function RootLayout({
   return (
     <html lang={lang == 'ru' ? lang : 'uk'}>
       <head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;500;600;700;800;900&display=swap"
+        />
         {/*<link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
           crossOrigin="anonymous"
-        />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;500;600;700;800;900&display=swap"
         />
 
         <Script
