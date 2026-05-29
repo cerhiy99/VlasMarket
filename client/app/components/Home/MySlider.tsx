5930450d4ab7039ca55b20ca2db152cbbede2685
cerhiy99 <cerhiy99@gmail.com>
2026-05-29 11:47:49 +0300

    uptomized baners

--- client/app/components/Home/MySlider.tsx
+++ client/app/components/Home/MySlider.tsx
@@ -1,93 +1,91 @@
 'use client';
+
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Autoplay } from 'swiper/modules';
+
 import 'swiper/css';
 import 'swiper/css/pagination';
-import 'swiper/css/autoplay';
+
 import './MySlider.scss';
 
-import { Pagination, Autoplay } from 'swiper/modules';
 import Image from 'next/image';
 import Link from 'next/link';
+
 import { getLocalizedPath } from '../utils/getLocalizedPath';
 import { Locale } from '@/i18n.config';
 
-const MySlider = ({
-  images,
-  lang,
-}: {
-  images: {
-    id: number;
-    mobileImg_uk: string;
-    mobileImg_ru: string;
-    href: string | null;
-    pcImg_ru: string;
-    pcImg_uk: string;
-  }[];
+type SliderImage = {
+  id: number;
+  mobileImg_uk: string;
+  mobileImg_ru: string;
+  pcImg_ru: string;
+  pcImg_uk: string;
+  href: string | null;
+};
+
+type Props = {
+  images: SliderImage[];
   lang: Locale;
-}) => {
+};
+
+const MySlider = ({ images, lang }: Props) => {
   return (
     <div className="my-swiper-container">
       <Swiper
         modules={[Pagination, Autoplay]}
-        spaceBetween={50}
         slidesPerView={1}
         pagination={{ clickable: true }}
         autoplay={{
-          delay: 10000, // Автоматичне перемикання кожні 10 секунд
-          disableOnInteraction: false, // Слайдер продовжує автоплей після взаємодії
+          delay: 10000,
+          disableOnInteraction: false,
         }}
+        loop={images.length > 1}
       >
-        {images.map((x, idx) => (
-          <SwiperSlide key={idx}>
-            {!x.href ? (
-              <>
+        {images.map((item, idx) => {
+          const mobileUrl =
+            process.env.NEXT_PUBLIC_SERVER +
+            item[`mobileImg_${lang === 'ru' ? 'ru' : 'uk'}`];
+
+          const desktopUrl =
+            process.env.NEXT_PUBLIC_SERVER +
+            item[`pcImg_${lang === 'ru' ? 'ru' : 'uk'}`];
+
+          const content = (
+            <div className="slide-image-wrapper">
+              <picture>
+                <source media="(max-width: 768px)" srcSet={mobileUrl} />
+
                 <Image
-                  style={{ objectFit: 'cover' }}
-                  src={
-                    process.env.NEXT_PUBLIC_SERVER + x[`mobileImg_${lang == 'ru' ? 'ru' : 'uk'}`]
-                  }
-                  fill
+                  src={desktopUrl}
                   alt={`Slide ${idx + 1}`}
-                  className="mob-img"
-                  priority={idx === 0}
-                  unoptimized={true}
-                />
-                <Image
-                  style={{ objectFit: 'cover' }}
-                  src={process.env.NEXT_PUBLIC_SERVER + x[`pcImg_${lang == 'ru' ? 'ru' : 'uk'}`]}
                   fill
-                  alt={`Slide ${idx + 1}`}
-                  className="pc-img"
                   priority={idx === 0}
-                  unoptimized={true}
+                  sizes="100vw"
+                  style={{
+                    objectFit: 'cover',
+                  }}
                 />
-              </>
-            ) : (
-              <Link href={getLocalizedPath(`/${lang}/${x.href}`, lang).replace('//', '/')}>
-                <Image
-                  style={{ objectFit: 'cover' }}
-                  src={
-                    process.env.NEXT_PUBLIC_SERVER + x[`mobileImg_${lang == 'ru' ? 'ru' : 'uk'}`]
-                  }
-                  fill
-                  alt={`Slide ${idx + 1}`}
-                  className="mob-img"
-                  priority={idx === 0}
-                  unoptimized={true}
-                />
-                <Image
-                  style={{ objectFit: 'cover' }}
-                  src={process.env.NEXT_PUBLIC_SERVER + x[`pcImg_${lang == 'ru' ? 'ru' : 'uk'}`]}
-                  fill
-                  alt={`Slide ${idx + 1}`}
-                  className="pc-img"
-                  priority={idx === 0}
-                  unoptimized={true}
-                />
-              </Link>
-            )}
-          </SwiperSlide>
-        ))}
+              </picture>
+            </div>
+          );
+
+          return (
+            <SwiperSlide key={item.id}>
+              {item.href ? (
+                <Link
+                  href={getLocalizedPath(`/${lang}/${item.href}`, lang).replace(
+                    '//',
+                    '/'
+                  )}
+                >
+                  {content}
+                </Link>
+              ) : (
+                content
+              )}
+            </SwiperSlide>
+          );
+        })}
       </Swiper>
     </div>
   );
