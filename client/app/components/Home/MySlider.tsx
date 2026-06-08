09ca85e567f80c2ff2a1ed968c07eb24f17c3591
cerhiy99 <cerhiy99@gmail.com>
2026-06-08 21:34:39 +0300

    add mob baner

--- client/app/components/Home/MySlider.tsx
+++ client/app/components/Home/MySlider.tsx
@@ -1,6 +1,7 @@
 import { Locale } from '@/i18n.config';
 import SliderClient from './SliderClient'; // Створимо нижче
 import './MySlider.scss'
+import Image from 'next/image';
 
 type SliderImage = { id: number; mobileImg_uk: string; mobileImg_ru: string; pcImg_ru: string; pcImg_uk: string; href: string | null; };
 
@@ -21,10 +22,25 @@ export default function MySlider({ images, lang }: { images: SliderImage[], lang
         <div className="swiper-wrapper">
           {/* Рендеримо перший слайд статично для миттєвого відображення */}
           <div className="swiper-slide" style={{ flex: '0 0 100%' }}>
-            <img 
+            <Image
+              width={888}
+              height={500}
               src={process.env.NEXT_PUBLIC_SERVER + (lang === 'ru' ? firstSlide.pcImg_ru : firstSlide.pcImg_uk)} 
               alt="banner" 
               className="pc-img"
+              priority
+              loading="eager"
+              unoptimized
+            />
+            <Image
+              width={400}
+              height={200}
+              src={process.env.NEXT_PUBLIC_SERVER + (lang === 'ru' ? firstSlide.mobileImg_ru : firstSlide.mobileImg_uk)} 
+              alt="banner" 
+              className="mob-img"
+              priority
+              loading="eager"
+              unoptimized
             />
             {/* Додайте аналогічно мобільну версію, якщо потрібно */}
           </div>
